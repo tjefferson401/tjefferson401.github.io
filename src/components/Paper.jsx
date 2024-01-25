d9ba9ebe83eacb19cc29129c28a24737350fa63f
TJ Jefferson <tjefferson4040@gmail.com>
2024-01-25 00:00:28 -0800

    added star

--- src/components/Paper.jsx
+++ src/components/Paper.jsx
@@ -1,4 +1,4 @@
-
+import { FaStar } from "react-icons/fa";
 
 
 export const Paper = ({authors, title, conference, picUrl, award=""}) => {
@@ -7,15 +7,27 @@ export const Paper = ({authors, title, conference, picUrl, award=""}) => {
         marginTop: "2px"
     }
 
+    const badgeStyle = {
+        position: 'absolute',
+        top: '0',
+        right: '0',
+        backgroundColor: 'gold',
+        padding: '2px 5px',
+        borderRadius: '0 0 0 5px', // Rounded corner on the bottom-left
+        fontSize: '12px',
+        fontWeight: 'bold',
+    };
 
     return (
         <div className="paper">
+            {award && <div style={badgeStyle}><FaStar/> {award}</div>}
             <img className="paper-pic" src={picUrl} alt={title}/>
             <div className="paper-info">
                 <h4 style={style}>{title}</h4>
                 <p style={style}>{authors}</p>
                 <p style={style}><i>{conference}</i></p>
             </div>
+
 
         </div>
     )
